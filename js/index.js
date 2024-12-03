c8c9c8396f05a619e0399a80ab3415254760a780
JoshStaff99 <Joshua.Stafford@netmatters-scs.com>
2024-12-03 14:21:23 +0000

    file reorganization and styling.
    Reorganized files to be more clear and applied styling to the drop down menu section.

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// ==========================================================================
+// Index JavaScript
+// ==========================================================================
+
+// Typing Effect
+var typed = new Typed('#banner-typing-1', {
+    strings: ['Hello my name Joshua Stafford'],
+    typeSpeed: 50,
+  });
+var typed = new Typed('#banner-typing-2', {
+    strings: ['I am a Web Developer'],
+    typeSpeed: 50,
+  }); 
+

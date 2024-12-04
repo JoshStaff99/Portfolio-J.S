0ad1df7e033534a943a69cebd2a9c030ed1b1b19
JoshStaff99 <Joshua.Stafford@netmatters-scs.com>
2024-12-04 16:29:39 +0000

    Phone validation and jQuery import.
    Phone validator now works as intended

--- js/index.js
+++ js/index.js
@@ -4,40 +4,20 @@
 
 // Typing Effect for the banner section of the landing page
 var typed = new Typed('#banner-typing-1', {
-    strings: ['Hello, My name is Joshua Stafford'],
+    strings: ['Hello, my name is Joshua Stafford'],
     typeSpeed: 50,
   });
 var typed = new Typed('#banner-typing-2', {
     strings: ['I am a Web Developer'],
     typeSpeed: 50,
-  }); 
+}); 
 
 // Removes the banner placeholder HTML content if JavaScript is active on the page
 document.querySelector(".banner-content-nojs").remove();
 
-// // Email validator 
-// const validateEmail = (email) => {
-//   return email.match(
-//     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-//   );
-// };
-
-// const validate = () => {
-//   const $result = $('#result');
-//   const email = $('#email').val();
-//   $result.text('');
-
-//   if(validateEmail(email)){
-//     $result.text(email + ' is valid.');
-//     $result.css('color', 'green');
-//   } else{
-//     $result.text(email + ' is invalid.');
-//     $result.css('color', 'red');
-//   }
-//   return false;
-// }
-
-// $('#email').on('input', validate);
+// Email validator 
+const $form_TelNo = $('#telNo'); 
+let isValidTelNo = ()=> /^\+?[0-9]{0,3}[-\s\.]?\(?[0-9]{3}\)?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test($form_TelNo.val());
 
 // Checks if fields are empty in form elements
 function validateForm() {
@@ -51,35 +31,37 @@ function validateForm() {
   if (firstnameForm == "") {
     alert("First Name must be filled out");
     return false;
-  } if (lastnameForm == "") {
+  } else if (lastnameForm == "") {
     alert("Last name must be filled out");
    return false;
-  } if (phonenumberForm == "") {
+  } else if (phonenumberForm == "") {
     alert("Phone number must be filled out");
    return false;
-  } if (emailaddressForm == "") {
+  } else if (!isValidTelNo()) {
+    alert("Not valid");
+    return false;
+  } else if (emailaddressForm == "") {
     alert("Email Address must be filled out");
    return false;
-  } if (subjectForm == "") {
+  } else if (subjectForm == "") {
     alert("Subject must be filled out");
    return false;
-  } if (messageForm == "") {
+  } else if (messageForm == "") {
     alert("Message must be filled out");
    return false;
-  } 
+  } else {
+    return true;
+  }
 }
 
 // Prevents the page from refreshing when you click on the submit button
- document.getElementClassName("btn-submit").addEventListener('click', function(event){event.preventDefault();});
+const $submitBtn = $('.btn-submit');
 
- //Get form element
- var form=document.getElementById("formId");
- function submitForm(event){
-
-    //Preventing page refresh
+$submitBtn.on('click', function(event){
+  if (validateForm() ) {
+    alert('Valid')
     event.preventDefault();
- }
-
- //Calling a function during form submission.
- form.addEventListener('submit', submitForm);
+  } else
+  event.preventDefault();
+});
 
